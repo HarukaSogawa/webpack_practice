cad779d76d755c305325ea9852907ae0a3953791
h-sogawa <89685474+h-sogawa@users.noreply.github.com>
2022-03-15 13:28:09 +0900

    Improve file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //波括弧で囲うことによてプラグインの一部の機能のみ取り出す
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    filename: 'javascripts/main.js',
   },
   module: {
     rules: [
@@ -24,9 +25,12 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/main.css',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+    }),
+    new CleanWebpackPlugin(),
   ],
 }
